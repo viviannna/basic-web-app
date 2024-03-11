cd085f92e94e25d9247415e4321eb3dac1db3a5a
viviannna <lieu.vivianna@gmail.com>
2024-03-11 12:34:02 -0400

    added andrew id query

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrew id")) {
+    return (
+      "vlieu"
+    );
+  }
+
   return "";
 }
