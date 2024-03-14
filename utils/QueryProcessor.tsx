a30114f09809351358706da43212d459b0d15033
viviannna <lieu.vivianna@gmail.com>
2024-03-14 15:46:05 -0400

    name no question mark

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,7 +13,7 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
-  if (query.toLowerCase().includes("what is your name?")) {
+  if (query.toLowerCase().includes("what is your name")) {
     return (
       "vivianna"
     );
