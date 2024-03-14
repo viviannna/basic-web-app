3e8a01bdf2325923371aab1b25b069a2ca8c929c
viviannna <lieu.vivianna@gmail.com>
2024-03-14 15:44:30 -0400

    name

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,5 +13,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("what is your name?")) {
+    return (
+      "vivianna"
+    );
+  }
+
   return "";
 }
